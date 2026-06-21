e43da9c9f2168e4283b758cfe3f6a844f8f31ea4
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-21 08:07:12 +0800

    Update root endpoint response message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Test for today - 062026");
+app.MapGet("/", () => "1t Test for Today");
 
 app.Run();
