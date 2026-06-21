e36dca58da9d4a1d23d2467a0d445899fb2ca142
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-06-21 15:06:32 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Happy Father's Day!!!!");
+app.MapGet("/", () => "Happy Sunday DBG Team");
 
 app.Run();
